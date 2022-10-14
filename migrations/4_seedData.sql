9cc334d06b9521a7e3b2b54b28220fc00906472f
paola <swan.madariaga@gmail.com>
2022-10-14 14:25:10 -0400

    data-users-posts-comments

--- migrations/4_seedData.sql
+++ migrations/4_seedData.sql
@@ -1,1 +1,5 @@
+\c blog
 
+\COPY users(email) FROM 'C:\Users\swanm\intro_dev\programming-intro-lab-14-Paolita-Mada\migrations\1_createUsersTable.csv' csv HEADER;
+\COPY posts FROM 'C:\Users\swanm\intro_dev\programming-intro-lab-14-Paolita-Mada\migrations\2_createPostsTable.csv' csv HEADER;
+\COPY comments FROM 'C:\Users\swanm\intro_dev\programming-intro-lab-14-Paolita-Mada\migrations\3_Comments.csv' csv HEADER;
---
+++ text.sql
@@ -0,0 +1,9 @@
+C:/Users/swanm/intro_dev/programming-intro-lab-14-Paolita-Mada/migrations/0_createDatabase.sql
+
+C:/Users/swanm/intro_dev/programming-intro-lab-14-Paolita-Mada/migrations/1_createUsersTable.sql
+
+C:/Users/swanm/intro_dev/programming-intro-lab-14-Paolita-Mada/migrations/2_createPostsTable.sql
+
+C:/Users/swanm/intro_dev/programming-intro-lab-14-Paolita-Mada/migrations/3_createComments.sql
+
+C:/Users/swanm/intro_dev/programming-intro-lab-14-Paolita-Mada/migrations/4_seedData.sql
