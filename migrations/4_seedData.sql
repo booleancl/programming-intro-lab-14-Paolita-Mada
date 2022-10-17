08c0922f853face3cf7a9278a798bcefa050dbef
paola <swan.madariaga@gmail.com>
2022-10-17 15:01:16 -0400

    queries-posts-comments-lazy user

--- migrations/4_seedData.sql
+++ migrations/4_seedData.sql
@@ -1,5 +1,5 @@
 \c blog
 
-\COPY users(email) FROM 'C:\Users\swanm\intro_dev\programming-intro-lab-14-Paolita-Mada\migrations\1_createUsersTable.csv' csv HEADER;
-\COPY posts FROM 'C:\Users\swanm\intro_dev\programming-intro-lab-14-Paolita-Mada\migrations\2_createPostsTable.csv' csv HEADER;
-\COPY comments FROM 'C:\Users\swanm\intro_dev\programming-intro-lab-14-Paolita-Mada\migrations\3_Comments.csv' csv HEADER;
+\COPY users(email) FROM 'C:/Users/swanm/intro_dev/programming-intro-lab-14-Paolita-Mada/data/users.csv' csv HEADER;
+\COPY posts FROM 'C:/Users/swanm/intro_dev/programming-intro-lab-14-Paolita-Mada/data/posts.csv' csv HEADER;
+\COPY comments FROM 'C:/Users/swanm/intro_dev/programming-intro-lab-14-Paolita-Mada/data/comentarios.csv' csv HEADER;
--- text.sql
+++ text.sql
@@ -1,9 +1,12 @@
-C:/Users/swanm/intro_dev/programming-intro-lab-14-Paolita-Mada/migrations/0_createDatabase.sql
-
-C:/Users/swanm/intro_dev/programming-intro-lab-14-Paolita-Mada/migrations/1_createUsersTable.sql
-
-C:/Users/swanm/intro_dev/programming-intro-lab-14-Paolita-Mada/migrations/2_createPostsTable.sql
-
-C:/Users/swanm/intro_dev/programming-intro-lab-14-Paolita-Mada/migrations/3_createComments.sql
+C:/Users/swanm/intro_dev/programming-intro-lab-14-Paolita-Mada/queries/5_posts-with-users.sql
+C:/Users/swanm/intro_dev/programming-intro-lab-14-Paolita-Mada/data/users.csv
+C:/Users/swanm/intro_dev/programming-intro-lab-14-Paolita-Mada/data/posts.csv
 
 C:/Users/swanm/intro_dev/programming-intro-lab-14-Paolita-Mada/migrations/4_seedData.sql
+
+C:/Users/swanm/intro_dev/programming-intro-lab-14-Paolita-Mada/queries/5_posts-with-users.sql
+C:/Users/swanm/intro_dev/programming-intro-lab-14-Paolita-Mada/queries/6_comments-with-users.sql
+C:/Users/swanm/intro_dev/programming-intro-lab-14-Paolita-Mada/queries/7_lazy-user.sql
+
+
+
